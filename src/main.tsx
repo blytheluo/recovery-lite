0a2236f2e96231805fd089b68b08099923cb63e0
Hellofood <68576157+blytheluo@users.noreply.github.com>
2026-07-03 22:25:42 +0800

    feat: migrate saved recovery records on launch

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './migrateBook';
 import App from './RecoveryEnhancements';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
